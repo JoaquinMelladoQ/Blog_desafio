bccf4b7ad23298db45325d069514128ec5163c12
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-30 12:09:00 -0300

    added methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.group_by
   end
+
+  def dashbaord
+    @article = Article.new    
+  end
+
+  def create
+    @article = Article.create(title: params[:title], image_url: params[:image_url], content: params[:content])
+  end
+
 end
