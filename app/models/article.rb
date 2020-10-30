69598b2747f7e96edcb351437748b54a3dd2c04c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-30 13:55:16 -0300

    added a validation to censore

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,3 +3,7 @@ class Article < ApplicationRecord
     validates :image_url, presence: true
     validates :content, presence: true
 end
+
+def censore
+    self.content = self.content.split([]) {|contenido, value| contenido.push(value) if value != "latam"; contenido}.join(" ")
+end
