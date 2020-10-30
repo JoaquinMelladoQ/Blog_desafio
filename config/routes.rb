ec8874a5fde542ce3d2513cade8d60832d90aa9a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-30 12:56:36 -0300

    added post to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
   get 'articles/dashboard'
-  get 'articles/create'
+  post 'articles/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
